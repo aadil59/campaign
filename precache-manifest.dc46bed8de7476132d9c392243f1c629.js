bdf6a48379dc6919a64f7ee4e6edbe31421db835
Aadil Khan <aadil.khan@nagarro.com>
2019-10-21 03:31:56 +0530

    Updates

---
+++ precache-manifest.dc46bed8de7476132d9c392243f1c629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f3431b4e15b3183bcf4369134c91fd",
+    "url": "/campaign/index.html"
+  },
+  {
+    "revision": "80f42596cb28c9fb34f9",
+    "url": "/campaign/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "aed0ffcc1b1c4cd2ba02",
+    "url": "/campaign/static/css/main.d1a1a71a.chunk.css"
+  },
+  {
+    "revision": "80f42596cb28c9fb34f9",
+    "url": "/campaign/static/js/2.7c355213.chunk.js"
+  },
+  {
+    "revision": "aed0ffcc1b1c4cd2ba02",
+    "url": "/campaign/static/js/main.ecda673f.chunk.js"
+  },
+  {
+    "revision": "a9021713690ad3b92abf",
+    "url": "/campaign/static/js/runtime-main.f1b29f0f.js"
+  },
+  {
+    "revision": "f5323dc0959819d9fe7bc3529927cd73",
+    "url": "/campaign/static/media/logo.f5323dc0.jpg"
+  }
+]);
